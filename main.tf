a5de6b6d6a0a66e3adeaffd03c36721e9f482be0
Grant Orchard <go@hashicorp.com>
2021-07-20 10:46:13 +1000

    provider config

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
+provider "aws" {
+	access_key = data.vault_aws_access_credentials.this.access_key
+	secret_key = data.vault_aws_access_credentials.this.secret_key
+	token = data.vault_aws_access_credentials.this.security_token
+}
+provider "vault" {
+	address = locals.vault_url
+	namespace = "admin"
+}
+
+locals {
+	vault_url = data.terraform_remote_state.terraform-hcp-core.vault_url
+}
+
+data "vault_aws_access_credentials" "this" {
+	backend = aws
+	type = "sts"
+	role = "personal"
+	ttl = "15m"
+}
+
+
 
